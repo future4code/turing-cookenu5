4058f066324931dbb261403f58ca5defd59b9bcb
Anna Fernandes <anna.cbf@gmail.com>
2020-09-16 16:44:07 -0300

    Ajustei funcionalidades

--- src/services/Authenticator.ts
+++ src/services/Authenticator.ts
@@ -1,32 +1,33 @@
 import * as jwt from 'jsonwebtoken';
 
 export class Authenticator {
-    public generateToken(data:AuthenticatonData): string {
-        return jwt.sign(
-            data,
-            process.env.JWT_KEY as string,
-            { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN }
-        )
-    }
+  public generateToken(data: AuthenticationData): string {
+    return jwt.sign(
+      data,
+      process.env.JWT_KEY as string,
+      {expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN}
+     )
+  }
 
-    public getData(token: string): AuthenticatonData {
-        const data = jwt.verify(
-            token,
-            process.env.JWT_KEY as string
-        ) as any;
-        return {
-            id: data.id,
-            role: data.role
-        }
+  public getData(token: string): AuthenticationData {
+    const data = jwt.verify(
+      token,
+      process.env.JWT_KEY as string
+    ) as any;
+    return {
+      id: data.id,
+      role: data.role
     }
+  }
 }
 
-export interface AuthenticatonData {
-    id: string,
-    role: USER_ROLES
+
+export interface AuthenticationData {
+  id: string;
+  role: USER_ROLES
 }
 
 export enum USER_ROLES {
-    ADMIN = "ADMIN",
-    NORMAL = "NORMAL"
+  ADMIN = 'ADMIN',
+  NORMAL = 'NORMAL'
 }
